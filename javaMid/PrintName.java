103bd83e618df325bf595727ea911b702f7918fb
jinwoo0710 <80086169+jinwoo0710@users.noreply.github.com>
2024-10-21 11:19:24 +0900

    Delete javaMid directory

--- javaMid/PrintName.java
+++
@@ -1,9 +0,0 @@
-package oop;
-
-class PrintName {
-    public static void main(String[] args) {
-        System.out.println("김진우");
-        System.out.println("20살");
-        System.out.println("컴퓨터소프트웨어과");
-    }
-}

f97db54e9477954dd7a2c75aa89ae80a703af2e8
jinwoo0710 <80086169+jinwoo0710@users.noreply.github.com>
2024-10-27 14:56:09 +0900

    Update Report3.java

--- javaMid/point/Report3.java
+++ javaMid/point/Report3.java
@@ -3,22 +3,10 @@ package report;
 
 class ColorPoint extends Point{
 	String color;
-	ColorPoint(int x, int y, String color) {
-		super(x, y);
-		this.color = color;
-	}
-	
-	void setXY(int x, int y) {
-		move(x, y);
-	}
-	
-	void setColor(String color) {
-		this.color = color;
-	}
-	
-	public String toString() {
-		return color + "색의 (" + getX() + "," + getY() + ")의 점";  
-	}
+	ColorPoint(int x, int y, String color) {super(x, y); this.color = color;}
+	void setXY(int x, int y) {move(x, y);}
+	void setColor(String color) {this.color = color;}
+	public String toString() {return color + "색의 (" + getX() + "," + getY() + ")의 점";}
 }
 
 public class Report3 {
